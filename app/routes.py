cac98a70644d5d3b6055d2bf311d458bfc50a350
Natalie Salazar <natmsal33@gmail.com>
2024-03-03 22:26:54 -0700

    WHAT I CHANGED: I connected to my local PostgreSQL db using psycopg2 in my Flask API located in the app folder of my repo. I then implemented the Flask routes "/user" which returns information of a single user according to a URL string query, and "/getAllUsers" which returns all users and user data from the PostgreSQL database.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+import psycopg2
 
 app = Flask(__name__)
+
+conn_string = "host='localhost' dbname='nataliesalazar'"
+
+# print ("Connecting to database\n	->%s" % (conn_string))
+
+# conn = psycopg2.connect(conn_string)
+
+# cursor = conn.cursor()
+
+# cursor.execute("""
+#     INSERT INTO users (id, name) VALUES
+#     (default,'Ben');
+# """) 
+
+# conn.commit()
+# conn.close()
+
+	# retrieve the records from the database
+	# records = cursor.fetchall()
+     
+    
+
+	# print out the records using pretty print
+	# note that the NAMES of the columns are not shown, instead just indexes.
+	# for most people this isn't very useful so we'll show you how to return
+	# columns as a dictionary (hash) in the next example.
+	# print(records)
 
 
 
@@ -12,13 +40,33 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+# Retrieve user data from database, throw error if user is not found.
 @app.route('/user')
 def getUser():
-    return "User Info"
+    # If arguments are passed into the URL, proceed to retrieve from database.
+    if request.args:
+        username = request.args.get("username", None)
+        conn = psycopg2.connect(conn_string)
+        cursor = conn.cursor()
+        params = {'username': username}
 
-@app.route('/getUsers')
+        cursor.execute("SELECT * FROM users WHERE name=%(username)s", params) 
+
+        userfetched = cursor.fetchall()
+        conn.close()
+    else: 
+        userfetched = "No user information given to search. Please try again."
+    return userfetched
+
+@app.route('/getAllUsers')
 def getUsers():
-    return "users" 
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    conn.close()
+
+    return users
 
 @app.route('/create-user')
 def createUser():
