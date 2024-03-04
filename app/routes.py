7b3a66323c6c7fa5441beb6d3fb9373949b87aef
Natalie Salazar <natmsal33@gmail.com>
2024-03-03 21:07:21 -0700

    WHAT CHANGED: The bare bones of a Flaks API have been outlined using @app.route() to designate the various pages I predict the app will need for data reading and writing

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,33 @@
-from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
+@app.route('/user')
+def getUser():
+    return "User Info"
+
+@app.route('/getUsers')
+def getUsers():
+    return "users" 
+
+@app.route('/create-user')
+def createUser():
+    return "User Created"
+ 
+@app.route('/get-messages')
+def getMessages():
+    return "messages"
+
+@app.route('/send-message')
+def sendMessage():
+    return "message sent"
